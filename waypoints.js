a9e6c7c443440758e56fb6d4667ebb7b8aca2484
Danny Chiang <dchiang720@gmail.com>
2017-11-24 20:39:32 -0500

    Add files via upload

---
+++ waypoints.js
@@ -0,0 +1,24 @@
+var $qr	 = $('.qr');
+
+$qr.waypoint(function () {
+	$qr.addClass('js-qr-animate');
+}, {offset: '45 %'});
+
+
+var $qr2 = $('.qr2');
+
+$qr2.waypoint(function () {
+	$qr2.addClass('js-qr-animate');
+}, {offset: '55 %'});
+
+var $qr3 = $('.qr3');
+
+$qr3.waypoint(function () {
+	$qr3.addClass('js-qr-animate');
+}, {offset: '45 %'});
+
+var $qr4 = $('.qr4');
+
+$qr4.waypoint(function () {
+	$qr4.addClass('js-qr-animate');
+}, {offset: '60 %'});
